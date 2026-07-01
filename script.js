9e00b876a0300b74ed02a7f06a40ae9bf423774f
otaviosobreira <osrodrigues47@gmail.com>
2026-07-01 06:20:18 -0300

    JS adicionar skills

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const botaoMensagem = document.querySelector('.btn-mensagem');
 const formularioContato = document.querySelector('.formulario-contato');
 const inputNome = document.querySelector('.input-nome');
 const feedback = document.querySelector('.feedback');
+const listaSkills = document.querySelector('.lista-skills');
+const skillsPadrao = ['HTML', 'CSS', 'JavaScript', 'FlexBox'];
+const formSkill = document.querySelector('.formSkill');
+const inputSkill = document.getElementById('novaSkill');
+let skills = skillsPadrao;
 
 botaoMensagem.addEventListener('click', ()=> {
     formularioContato.classList.toggle('escondido');
@@ -22,3 +27,42 @@ formularioContato.addEventListener('submit', (event) =>{
     `;
     formularioContato.reset();
 });
+
+const renderizarSkills = () => {
+    const skillsHTML = skills.map((skill)=>{
+        return `<li>${skill}</li>`;
+    });
+
+    listaSkills.innerHTML = skillsHTML.join('');
+};
+
+const skillsSalvas = localStorage.getItem('skills');
+
+if(skillsSalvas){
+    skills = JSON.parse(skillsSalvas);
+}
+
+const salvarSkills = () => {
+    localStorage.setItem('skills', JSON.stringify(skills));
+}
+
+formSkill.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const novaSkill = inputSkill.value.trim();
+
+    if(!novaSkill){
+        return;
+    }
+
+    skills.push(novaSkill);
+    
+    salvarSkills();
+    renderizarSkills();
+
+    formSkill.reset();
+    inputNome.focus();
+})
+
+salvarSkills();
+
+renderizarSkills();
